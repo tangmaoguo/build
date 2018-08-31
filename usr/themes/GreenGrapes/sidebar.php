a6efe8b748761643722e42ceabee6e6d36fce3ab
tangmaoguo <1096301477@qq.com>
2018-09-01 00:10:06 +0800

    登陆页面

--- usr/themes/GreenGrapes/sidebar.php
+++ usr/themes/GreenGrapes/sidebar.php
@@ -1,4 +1,43 @@
+
 <aside id="sidebar">
+    <aside>
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-info btn-lg " data-toggle="modal" data-target="#myModal" style="width:100%;margin-bottom:20px;">
+            登陆
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" >
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                        <h4 class="modal-title" id="myModalLabel">用户登陆（该功能暂未开放）</h4>
+                    </div>
+                    <form action="" method="post" name="login" role="form" class="form-horizontal">
+                        <div class="modal-body">
+                            <div class="form-group">
+                            <div class="col-sm-6">
+                                <label for="name" class="sr-only"><?php _e('用户名'); ?></label>
+                                <input type="text" id="name" name="name" value="<?php echo $rememberName; ?>" placeholder="<?php _e('用户名'); ?>" class="form-control" autofocus />
+                            </div>
+                            </div>
+                            <div class="form-group">
+                            <div class="col-sm-6">
+                                <label for="password" class="sr-only"><?php _e('密码'); ?></label>
+                                <input type="password" id="password" name="password" placeholder="<?php _e('密码'); ?>"class="form-control" />
+                            </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer" style="text-align: center;">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+                            <button type="button" class="btn btn-primary">确认登陆</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </aside>
     <aside>
         <form method="get" id="searchform" class="form-inline clearfix" action="./">
             <input class="form-control" name="s" id="s" placeholder="搜索关键词..." type="text">
@@ -24,7 +63,7 @@
                     <?php while($comments->next()): ?>
                         <li class="list-group-item clearfix"><?php $comments->author(false); ?>：<a href="<?php $comments->permalink(); ?>" target="_blank"><?php $comments->excerpt(35, '...'); ?></a></li>
                     <?php endwhile; ?>
-                        </ul>
+                    </ul>
                 </div>
                 <div class="tab-pane nav bs-sidenav fade" id="sidebar-rand">
                     <?php theme_random_posts();?>
@@ -33,35 +72,35 @@
         </div>
     </aside>
     <?php if(class_exists('Links_Plugin') && isset($this->options->plugins['activated']['Links'])): ?>
-    <aside>
-        <div class="panel panel-green hidden-xs">
-            <div class="panel-heading"><i class="fa fa-link fa-fw"></i> 友情链接</div>
-            <ul class="list-group">
-                <?php Links_Plugin::output('<li class="list-group-item"><a href="{url}" target="_blank">{name}</a></li>', 10, NULL, true); ?>
-            </ul>
-        </div>
-    </aside>
+        <aside>
+            <div class="panel panel-green hidden-xs">
+                <div class="panel-heading"><i class="fa fa-link fa-fw"></i> 友情链接</div>
+                <ul class="list-group">
+                    <?php Links_Plugin::output('<li class="list-group-item"><a href="{url}" target="_blank">{name}</a></li>', 10, NULL, true); ?>
+                </ul>
+            </div>
+        </aside>
     <?php endif; ?>
     <?php if(false): ?>
-    <aside>
-        <div class="panel panel-green hidden-xs">
-            <div class="panel-heading"><i class="fa fa-book fa-fw"></i> 文章分类</div>
-            <div class="list-group category">
-                <?php $this->widget('Widget_Metas_Category_List')->listCategories('wrapClass=widget-list'); ?>
+        <aside>
+            <div class="panel panel-green hidden-xs">
+                <div class="panel-heading"><i class="fa fa-book fa-fw"></i> 文章分类</div>
+                <div class="list-group category">
+                    <?php $this->widget('Widget_Metas_Category_List')->listCategories('wrapClass=widget-list'); ?>
+                </div>
             </div>
-        </div>
-    </aside>
+        </aside>
     <?php endif; ?>
     <div id="fixed"></div>
     <aside class="fixsidebar">
         <div class="panel panel-green hidden-xs">
             <div class="panel-heading"><i class="fa fa-tags fa-fw"></i> 标签云</div>
             <div id="meta-cloud">
-            <canvas height="300" id="mycanvas" style="width: 100%">
-                <p>标签云</p>
-                <?php $this->widget('Widget_Metas_Category_List')->listCategories('wrapClass=widget-list'); ?>
-                <?php $this->widget('Widget_Metas_Tag_Cloud')->parse('<a href="{permalink}" class="tag">{name}</a>'); ?>
-            </canvas>
+                <canvas height="300" id="mycanvas" style="width: 100%">
+                    <p>标签云</p>
+                    <?php $this->widget('Widget_Metas_Category_List')->listCategories('wrapClass=widget-list'); ?>
+                    <?php $this->widget('Widget_Metas_Tag_Cloud')->parse('<a href="{permalink}" class="tag">{name}</a>'); ?>
+                </canvas>
             </div>
         </div>
     </aside>
