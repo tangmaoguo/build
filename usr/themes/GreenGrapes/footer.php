eba8d1bfa38381b2a5dae111005f871f40fcfe92
tangmaoguo <123@qq.com>
2018-10-16 11:30:01 +0800

    Revert "Revert "去除https""
    This reverts commit 87b12c65495e356ba84a437f4472924c4947e808.

--- usr/themes/GreenGrapes/footer.php
+++ usr/themes/GreenGrapes/footer.php
@@ -27,13 +27,6 @@
 <script src = "<?php $this->options->themeUrl('js/headerCanvas.js'); ?>"></script>
 <script src = "<?php $this->options->themeUrl('js/home.js'); ?>"></script>
 <?php $this->footer(); ?>
-<script>
-    $(function(){
-        if(location.href.indexOf('www') == -1){
-           host =  window.location.host
-            location.href = "https://www."+host;
-        }
-    })
-</script>
+
 </body>
 </html>
