9c2228c49c85cf47ac25937961b234d7e1b74c3c
tangmg <123@qq.com>
2019-03-08 18:47:58 +0800

    收录

--- usr/themes/GreenGrapes/footer.php
+++ usr/themes/GreenGrapes/footer.php
@@ -35,6 +35,26 @@
 <script src="<?php $this->options->themeUrl('js/particles.min.js'); ?>"></script>
 <script src="<?php $this->options->themeUrl('js/headerCanvas.js'); ?>"></script>
 <script src="<?php $this->options->themeUrl('js/home.js'); ?>"></script>
+<script>
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>
+<script>
+    (function(){
+        var src = (document.location.protocol == "http:") ? "http://js.passport.qihucdn.com/11.0.1.js?661d9dbbf31a31db723ba11e068c32ca":"https://jspassport.ssl.qhimg.com/11.0.1.js?661d9dbbf31a31db723ba11e068c32ca";
+        document.write('<script src="' + src + '" id="sozz"><\/script>');
+    })();
+</script>
 <?php $this->footer(); ?>
 </body>
 </html>
